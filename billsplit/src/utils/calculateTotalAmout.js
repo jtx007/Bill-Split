790e0b4cb52448d4b1241f3a0db0fb0e05b092ed
jtx007 <jamesjacobthomas7@gmail.com>
2020-04-02 15:48:45 -0400

    fixed decimal point for calculations

--- billsplit/src/utils/calculateTotalAmout.js
+++ billsplit/src/utils/calculateTotalAmout.js
@@ -6,22 +6,22 @@ export const calculateLogisticsTotal = bill => {
       parseInt(bill.amountTotal) / parseInt(bill.numberOfPartyMembers);
     let tipPerPerson = parseInt(bill.amountTotal) * (parseInt(bill.tipPercent) / 100);
     let amountPerPerson = initial + tipPerPerson;
-    return amountPerPerson;
+    return amountPerPerson.toFixed(2)
   
   } else if (bill.tipPercent.length === 0) {
     let taxAmount = parseInt(bill.amountTotal) * (parseInt(bill.taxPercent) / 100);
     let initial = parseInt(bill.amountTotal) + taxAmount
     let amountPerPerson = (initial / (parseInt(bill.numberOfPartyMembers)))
-    return amountPerPerson
+    return amountPerPerson.toFixed(2)
   
   } else if (!bill.tipPercent && !bill.taxPercent) {
     let amountPerPerson = parseInt(bill.amountTotal) / parseInt(bill.numberOfPartyMembers);
-    return amountPerPerson
+    return amountPerPerson.toFixed(2)
   } else {
     let initial = parseInt(bill.amountTotal) / parseInt(bill.numberOfPartyMembers);
     let taxAmount = parseInt(bill.amountTotal) * (parseInt(bill.taxPercent) / 100);
     let tipPerPerson = (parseInt(bill.amountTotal) + taxAmount) * (parseInt(bill.tipPercent) / 100);
     let amountPerPerson = (initial + taxAmount) + tipPerPerson 
-    return amountPerPerson
+    return amountPerPerson.toFixed(2)
   }
 };
